444c6000c45f90958e583f6d17bd2d2f5250fbc6
rumi <rumi@rumi-no-MacBook-Pro.local>
2013-12-21 23:37:29 +0900

    Initial Commit

---
+++ ASAppLikeSiri2/ASViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASViewController.h
+//  ASAppLikeSiri2
+//
+//  Created by rumi on 2013/12/21.
+//  Copyright (c) 2013年 Mocology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASViewController : UIViewController
+
+@end
